046187b78ea16eb4b20ab5b71fb87f9e355c9897
hyemin065 <p62517@gmail.com>
2022-05-13 03:49:55 +0900

    fix infinity scroll

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,5 +1,5 @@
 import MoviesItem from 'components/MoviesItem'
-import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { getMoviesApi } from 'services/movies'
 import { movieDataState, movieInputState, moviePageState } from 'store/movies'
@@ -8,50 +8,39 @@ import styles from './search.module.scss'
 const Search = () => {
   const [movies, setMovies] = useRecoilState(movieDataState)
   const [pageNumber, setPageNumber] = useRecoilState(moviePageState)
-  // const [isLoading, setIsLoading] = useState(true)
   const inputValue = useRecoilValue(movieInputState)
-  const ref = useRef<HTMLDivElement>(null)
-
-  // const getMovies = async (page: number) => {
-  //   const res = await getMoviesApi({ title: inputValue, page })
-  //   const newMovies = res.Search
-  //   setMovies((movie) => [...movie, ...newMovies])
-  //   console.log('sdfsdf')
-  // }
+  const pageRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const apiCall = async () => {
+    const getMovies = async () => {
       if (pageNumber > 1) {
         const res = await getMoviesApi({ title: inputValue, page: pageNumber })
         const newMovies = res.Search
 
-        if (String(res.Response) === 'True' && res.Search) {
+        if (String(res.Response) === 'True' && newMovies) {
           setMovies((movie) => [...movie, ...newMovies])
         }
       }
     }
-    apiCall()
-  }, [pageNumber])
-
-  console.log('page', pageNumber)
-
-  // const loadMore = () => {
-  //   setPageNumber((prev) => prev + 1)
-  // }
+    getMovies()
+  }, [inputValue, pageNumber, setMovies])
 
   useEffect(() => {
-    if (!ref.current) return
-    console.log(ref.current)
-
-    const observer = new IntersectionObserver((entries) => {
+    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
       if (entries[0].isIntersecting) {
         setPageNumber((prev) => prev + 1)
       }
     })
-    // if (ref.current) {
-    observer.observe(ref.current)
-    // }
-  }, [movies])
+    if (pageRef.current) {
+      observer.observe(pageRef.current)
+    }
+
+    return () => {
+      if (pageRef.current) {
+        observer.unobserve(pageRef.current)
+      }
+    }
+  }, [movies, setPageNumber])
 
   return (
     <main className={styles.searchResultWrapper}>
@@ -66,7 +55,7 @@ const Search = () => {
               <MoviesItem key={item.imdbID} {...item} />
             ))}
           </ul>
-          <div ref={ref}>로딩로딩로딩로딩로딩로딩로딩로딩로딩로딩</div>
+          <div ref={pageRef}>Load More</div>
         </>
       )}
     </main>
